ed9cbf5722fb423374aadeaec008cd33268d07c2
batman <batman@pc.com>
2022-08-17 10:16:31 +0800

    update solution description

--- search_2d_matrix.py
+++ search_2d_matrix.py
@@ -1,13 +1,16 @@
-#
-# 1. The matrix(mxn) array is sorted.
-# 2. We can use binary search in row and cols
-# 
-#    m == matrix.length
-#    n == matrix[i].length
-#    1 <= m, n <= 100
-# search in rows(the first number in each row)
-#
 def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+'''
+The matrix(mxn) array is sorted, so we can refactor our solution to use binary search if we have enough time. 
+
+**Solution:**
+We can first locate the row in rows, and then search the row for target directly. 
+1. Here we use python's list comprehension to extract the rows array ( space complexity: m ), then loop it to find the row index for target.
+2. Since n <= 100, so we search for it directly with python's list search: "target in row"
+
+**Loop times:** m + m + m + n
+**Time complexity: O(m+n)**
+**Runtime:** 86 ms
+'''
 	m = len(matrix)
 	n = len(matrix[0])
 
